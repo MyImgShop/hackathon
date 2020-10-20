d3cfd39ff0dbbd9d853bb394525454c62d5699e8
mouad <mouaad99@hotmail.fr>
2020-10-20 12:35:09 +0200

    commit test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Footer  from './component/Footer'
 
 
 export default class App extends React.Component{
   render(){
     return(
-      <Footer />
+      <div>
+        hello test
+      </div>
     )
   }
 }

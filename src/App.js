cebae8be285967f22d129052953c18690ef8034b
Liam <liam.f.plummer@gmail.com>
2020-10-20 12:12:41 +0200

    html propre

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Footer  from './component/Footer'
+
 
 export default class App extends React.Component{
   render(){
     return(
-      
+      <Footer />
     )
   }
 }

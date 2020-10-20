f0827054fe3d752c4251117a54292a80892ed2b4
Liam <liam.f.plummer@gmail.com>
2020-10-20 12:24:52 +0200

    branch

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,16 +3,19 @@ import React from 'react';
 export default class Footer extends React.Component{
     render(){
         return(
-            <div className="row">
-                <div className="col-4 bg-dark text-grey">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae provident fugiat expedita minima, ullam fugit ipsam rerum laborum dolor quaerat. Quidem id enim quisquam vel!</p>
+            <div className="d-flex flex-column p-4 ">
+                <div className="row">
+                    <div className="col-4 p-2 mx-auto bg-dark text-white">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae provident fugiat expedita minima, ullam fugit ipsam rerum laborum dolor quaerat. Quidem id enim quisquam vel!</p>
+                    </div>
+                    <div className="col-4 p-2 mx-auto bg-dark text-white">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolore.</p>
+                    </div>
+                    <div className="col-4 p-2 mx-auto bg-dark text-white">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
                 </div>
-                <div className="col-4 bg-dark text-grey">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, dolore.</p>
-                </div>
-                <div className="col-4 bg-dark text-grey">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                </div>
+                <p className="bg-danger text-center"> &:copy Mouad &:copy Karis, &:copy Liam </p>
             </div>
         )   
     }

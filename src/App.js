5cc6575c134bc9efdd042646e054780e9af90118
mouad <mouaad99@hotmail.fr>
2020-10-21 12:28:01 +0200

    update page acceuil

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-
-
+import Navbar from './component/acceuil/Navbar.js';
 export default class App extends React.Component{
   render(){
+    let video = "./video/blackspace.mp4"
+
     return(
       <div>
-        hello test
+        <video autoPlay loop muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-2"
+        }}>
+          <source src={video} type="video/mp4"/>
+        </video>
+        <Navbar />
       </div>
     )
   }
